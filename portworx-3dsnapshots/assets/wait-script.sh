407760ef9185af880726df51b632c352db1ab5b1
wallnerryan <rwallner@purestorage.com>
2021-03-08 14:19:25 -0500

    change wait script to look for services not endpoints

--- portworx-3dsnapshots/assets/wait-script.sh
+++ portworx-3dsnapshots/assets/wait-script.sh
@@ -1,7 +1,7 @@
 clear
 
 echo -e "Please wait while we create some databases for this lab. It may take a few minutes..."
-until [ `kubectl get ep | egrep '3306|27017' | wc -l` -eq 2 ] ; do
+until [ `kubectl get svc | egrep '3306|27017' | wc -l` -eq 2 ] ; do
   printf '.'
   sleep 1
 done
