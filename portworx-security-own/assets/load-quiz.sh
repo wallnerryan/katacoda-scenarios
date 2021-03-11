3c60ca5e7ac5a50636bc42c35758ba2be8317f06
wallnerryan <rwallner@purestorage.com>
2021-03-11 14:15:44 -0500

    update image, update csi px spec

--- portworx-security-own/assets/load-quiz.sh
+++ portworx-security-own/assets/load-quiz.sh
@@ -5,13 +5,8 @@ launch.sh
 
 sh /tmp/create-pxkeys.sh
 
-##Create an etcd container
-#sh /tmp/install-etcd.sh
-
-
 ##Install Portworx with CSI
 cp /tmp/px-spec.yaml /root/px-spec.yaml
-
 
 kubectl delete ds kube-keepalived-vip -n kube-system
 
